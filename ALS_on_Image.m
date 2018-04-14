dea31990b650f33e0ae060ae43fab9547907b2cd
Aniket <aniketpokale10@gmail.com>
2018-04-14 15:32:55 +0530

    Corrected ALS_on_image.m

--- ALS_on_Image.m
+++ ALS_on_Image.m
@@ -3,7 +3,6 @@
 
 input = imread('macbeth_input1.jpg'); % input image is the image to be corrected
 original = imread('macbeth_original.jpg'); %%original image is how the corrected image should be 
-background = imread('macbeth_background.jpg');
 original = rgb2xyz(original); % We take XYZ color space as reference for mapping
 iterations = 15; % The alternating least squares algorithm converges in about 15 iterations 
 
@@ -12,7 +11,6 @@ for i=1:size(input,1)
     for j=1:size(input,2)
         P = [P; input(i,j,1), input(i,j,2), input(i,j,3)];
         Q= [Q; original(i,j,1), original(i,j,2), original(i,j,3)];
-        B = [B; background(i,j,1), background(i,j,2), background(i,j,3)];
     end
 end
 
@@ -20,13 +18,8 @@ B = double(B);
 P = double(P);
 n = length(P);
 
-for i=1:n
-    P(i,:) = P(i,:)./B(i,:);
-    P(i,:) = P(i,:).*255;
-end
-
 D = zeros(n);
-for k=1:50
+for k=1:15
     for j=1:n
         if (norm(P(j,:))^2) ~= 0
             D(j,j) = (P(j,:)*Q(j,:)')/(norm(P(j,:))^2); % calculating matrix of shading factors
@@ -38,7 +31,7 @@ for k=1:50
 
 end
 
-CCerrors(P*M,Q);
+CCerrors(P,Q);
 
 P = uint8(P);
 count = 1;
@@ -50,7 +43,7 @@ for i=1:size(input,1)
     end
 end
 
-original = xyz2rgb(original).*255;
+original = xyz2rgb(original,'OutputType','uint8');
 subplot(2,2,3); imshow(uint8(original)); title('Original');
 subplot(2,2,1);
 imshow(input); title('Intput image');
